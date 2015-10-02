ca5e48b26a39460e8433790f08ebfc6cce590851
Alex Hanson <ahanson@runbox.com>
2015-10-02 16:50:03 -0500

    Add descriptive comment above QA script.

--- QA.sh
+++ QA.sh
@@ -1,1 +1,2 @@
-hash xfce4-terminal && xfce4-terminal -e "./data/qa-menu.sh" || (hash gnome-terminal && gnome-terminal -e "./data/qa-menu.sh" || xterm -e "./data/qa-menu.sh")
+#Detect terminal on the system, then execute menu script in that terminal:
+hash xfce4 -terminal && xfce4-terminal -e "./data/qa-menu.sh" || (hash gnome-terminal && gnome-terminal -e "./data/qa-menu.sh" || xterm -e "./data/qa-menu.sh")
