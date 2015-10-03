51bd5df5b629d3417ae6189ad40189c5b69a7624
Alex Hanson <ahanson@runbox.com>
2015-10-02 22:40:58 -0500

    supressed command-line errors if certain terminals (xfce or gnome) aren't found.

--- QA.sh
+++ QA.sh
@@ -1,2 +1,2 @@
 #Detect terminal on the system, then execute menu script in that terminal:
-hash xfce4 -terminal && xfce4-terminal -e "./data/qa-menu.sh" || (hash gnome-terminal && gnome-terminal -e "./data/qa-menu.sh" || xterm -e "./data/qa-menu.sh")
+hash xfce4-terminal 2> /dev/null && xfce4-terminal -e "./data/qa-menu.sh" 2> /dev/null || (hash gnome-terminal 2> /dev/null && gnome-terminal -e "./data/qa-menu.sh" 2> /dev/null || xterm -e "./data/qa-menu.sh" 2> /dev/null ) 
